c0c06cfb3f79b9032658978e153abf344efd5786
Jonathan Cocks <joncocks@hotmail.com>
2023-06-21 16:20:48 -0700

    udpate gitignore

--- models/test_model.py
+++ models/test_model.py
@@ -1,7 +1,7 @@
 from models.models import IsPotatoModel
 from PIL import Image
 model = IsPotatoModel("classifier_model.h5", img_height=150, img_width=150)
-image1 = Image.open("C:\\Projects\\code\\is-potato\\app\\static\\potatoes\\test.jpg")
+image1 = Image.open("/app/static/potatoes/test.jpg")
 image2 = Image.open("C:\\Projects\\code\\is-potato\\app\\static\\potatoes\\not_potato.jpg")
 
 r1 = model.predict(image1)
